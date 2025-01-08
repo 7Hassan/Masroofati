e7228b3873f75fb3426d776ec4cf227ea9cfd2e7
Hassan Hossam <egoker1234@gmail.com>
2025-01-09 00:08:50 +0200

    colors

--- server/controller/variables.js
+++ server/controller/variables.js
@@ -1,5 +1,17 @@
 const daysData = ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
-const colorsData = ["#8A2BE2", "#00CED1", "#4682B4", "#9370DB"];
+const colorsData = [
+  '#6B8E23',
+  '#FF8C00',
+  '#4682B4',
+  '#9370DB',
+  '#FFD700',
+  '#B22222',
+  '#228B22',
+  '#708090',
+  '#8B4513',
+  '#DB7093'
+]
+
 
 module.exports = {
   daysData,
